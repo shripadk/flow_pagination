d53fdba29999a79d913c3928fee2ecefe2a13ee3
Dmitry Afanasyev <dimarzio1986@gmail.com>
2009-12-20 20:40:55 +0300

    link changed to plane link without js (for use with jquery)

--- lib/flow_pagination.rb
+++ lib/flow_pagination.rb
@@ -5,16 +5,20 @@ module FlowPagination
 
     # Render flow navigation
     def to_html
-      flow_pagination = ''
-
-      if self.current_page < self.last_page
-        flow_pagination = @template.button_to_remote(
-            @template.t('flow_pagination.button', :default => 'More'),
-            :url => { :controller => @template.controller_name,
-              :action => @template.action_name,
-              :params => @template.params.merge!(:page => self.next_page)},
-            :method => @template.request.request_method)
-      end
+      # flow_pagination = ''
+      flow_pagination = @template.link_to(
+        I18n.t('flow_pagination.link_text', :default => 'More'),
+        self.next_page.to_s, 
+        :id => 'flow_pager'
+      )
+      # if self.current_page < self.last_page
+      #   flow_pagination = @template.button_to_remote(
+      #       @template.t('flow_pagination.button', :default => 'More'),
+      #       :url => { :controller => @template.controller_name,
+      #         :action => @template.action_name,
+      #         :params => @template.params.merge!(:page => self.next_page)},
+      #       :method => @template.request.request_method)
+      # end
 
       @template.content_tag(:div, flow_pagination, :id => 'flow_pagination')
 
